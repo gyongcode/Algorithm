6982c6472722788dcf0c0c0b9ea130dcf04620d0
Lee Ji Yong <165859240+gyongcode@users.noreply.github.com>
2025-05-24 18:59:01 +0900

    [level 3] Title: 순위, Time: 7.76 ms, Memory: 84.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/49191. 순위/순위.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+class Solution {
+    static boolean[][] visited;
+    
+    
+    public int solution(int n, int[][] results) {
+        visited = new boolean[n+1][n+1];
+        for(int i=1; i<n+1; i++){
+            visited[i][i] = true;
+        }
+        for(int[] result : results){
+            visited[result[0]][result[1]] = true;
+        }
+        
+        fw(n);
+        
+        int result = 0;
+        
+        for(int i=1; i<n+1; i++){
+            int cnt = 0;
+            for(int j=1; j<n+1; j++){
+                if(visited[i][j] || visited[j][i]){
+                    cnt++;
+                }
+            }
+            if(cnt == n){
+                result++;
+            }
+        }
+        return result;
+    }
+    
+    static void fw(int n){
+        for(int k =1; k<n+1; k++){
+            for(int i=1; i<n+1; i++){
+                for(int j=1; j<n+1; j++){
+                    if(visited[i][k] && visited[k][j]){
+                        visited[i][j] = true;                    
+                    }
+                }
+            }
+        }
+    }
+}
