b6f84c86fc00d9d25f1ea317a8bdd140a632ccc9
Lee Ji Yong <165859240+gyongcode@users.noreply.github.com>
2025-07-07 09:16:18 +0900

    [Silver II] Title: 이항 계수 2, Time: 120 ms, Memory: 18292 KB -BaekjoonHub

---
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int N, K;
+    static final int MOD = 10007;
+
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        int[][] dp = new int[N + 1][N + 1];
+        for (int i = 0; i < N + 1; i++) {
+            dp[i][i] = 1;
+            dp[i][0] = 1;
+            dp[i][1] = 1;
+        }
+
+        for (int i = 2; i < N + 1; i++) {
+            for (int j = 1; j < i; j++) {
+                dp[i][j] = (dp[i - 1][j] + dp[i - 1][j - 1]) % MOD;
+            }
+        }
+
+        System.out.println(dp[N][K]);
+    }
+
+
+}
