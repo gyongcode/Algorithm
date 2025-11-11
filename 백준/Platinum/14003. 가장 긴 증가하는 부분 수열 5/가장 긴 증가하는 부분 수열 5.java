de275b67ce36d2ffed2e424cf90fef0d3f6057cf
Lee Ji Yong <165859240+gyongcode@users.noreply.github.com>
2025-11-11 11:13:03 +0900

    [Platinum V] Title: 가장 긴 증가하는 부분 수열 5, Time: 748 ms, Memory: 185340 KB -BaekjoonHub

--- 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.java
+++ 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.java
@@ -1,18 +1,17 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.ArrayDeque;
 import java.util.StringTokenizer;
 
 public class Main {
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static int N;
-    static int[] inputs, lis, dp;
+    static int[] inputs, dp, lens;
 
     public static void main(String[] args) throws IOException {
+
         N = Integer.parseInt(br.readLine());
         inputs = new int[N + 1];
 
@@ -21,53 +20,56 @@ public class Main {
             inputs[i] = Integer.parseInt(st.nextToken());
         }
 
+        int index = 0;
         dp = new int[N + 1];
-        lis = new int[N + 1];
-        lis[0] = -1_000_000_001;
-        int len = 0;
+        dp[0] = -1_000_000_001;
+        lens = new int[N + 1];
+
         for (int i = 1; i < N + 1; i++) {
-
-            if (inputs[i] > lis[len]) {
-                lis[++len] = inputs[i];
-                dp[i] = len;
+            if (dp[index] < inputs[i]) {
+                dp[++index] = inputs[i];
+                lens[i] = index;
             } else {
-                int idx = binarySearch(0, len, inputs[i]);
-                lis[idx] = inputs[i];
-                dp[i] = idx;
-
+                int tmp = binarySearch(0, index, inputs[i]);
+                dp[tmp] = inputs[i];
+                lens[i] = tmp;
             }
         }
+
+        System.out.println(index);
 
         StringBuilder sb = new StringBuilder();
-        sb.append(len).append("\n");
-
-        List<Integer> result = new ArrayList<>();
-        for (int i = N; i > 0; i--) {
-            if (dp[i] == len) {
-                result.add(inputs[i]);
-                len--;
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+        int lensIndex = N;
+        while (index > 0) {
+            if (lens[lensIndex] == index) {
+                stack.addLast(inputs[lensIndex]);
+                index--;
             }
+
+            lensIndex--;
         }
-        Collections.reverse(result);
-        for (Integer i : result) {
-            sb.append(i).append(" ");
+
+        while (!stack.isEmpty()) {
+            sb.append(stack.removeLast()).append(" ");
         }
+
         System.out.println(sb);
     }
 
-    static int binarySearch(int left, int right, int key) {
 
+    static int binarySearch(int left, int right, int key) {
         while (left < right) {
             int mid = (left + right) / 2;
 
-            if (lis[mid] < key) {
+            if (dp[mid] < key) {
                 left = mid + 1;
             } else {
                 right = mid;
             }
         }
 
-        return right;
+        return left;
     }
-
 }
+
